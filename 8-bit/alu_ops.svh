c85b3e6bb7a13475ba74e7c17503325f1f3cad62
Suijester <akilesh.yes@gmail.com>
2026-05-11 01:09:47 -0700

    header file

---
+++ 8-bit/alu_ops.svh
@@ -0,0 +1,10 @@
+typedef enum logic [2:0] {
+    ADD = 3'b00,
+    SUB = 3'b001,
+    RIGHT_SHIFT = 3'b010,
+    LEFT_SHIFT = 3'b011,
+    AND = 3'b100,
+    OR = 3'b101,
+    XOR = 3'b110,
+    WRITE = 3'b111
+} alu_ops;
